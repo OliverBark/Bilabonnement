4d3855531873db43bceea65ce046cc7b2f36055e
OliverBark <oliver.bark@icloud.com>
2022-05-12 11:18:13 +0200

    postmapping login

--- src/main/java/com/example/bilabonnement/controllers/login/LoginController.java
+++ src/main/java/com/example/bilabonnement/controllers/login/LoginController.java
@@ -1,9 +1,12 @@
 package com.example.bilabonnement.controllers.login;
 
+import com.example.bilabonnement.repositories.SQL_Manager;
 import com.example.bilabonnement.services.ControllerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
 
@@ -17,6 +20,24 @@ public class LoginController {
             return "login/logout";
         }
         return "login/login";
+    }
+    @PostMapping("/logging") //log into an existing user
+    public String logging(WebRequest dataFromForm, HttpSession session) {
+        SQL_Manager sql = new SQL_Manager();
+        sql.start();
+        try {
+            if (sql.login(dataFromForm.getParameter("username"), dataFromForm.getParameter("password"))) {
+                //successful login
+                session.setAttribute("username", dataFromForm.getParameter("username"));
+                return "redirect:/forside";
+            } else {
+                //invalid login
+                return "redirect:/login";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/login";
     }
 
 
--- src/main/java/com/example/bilabonnement/repositories/SQL_Manager.java
+++ src/main/java/com/example/bilabonnement/repositories/SQL_Manager.java
@@ -3,16 +3,47 @@ package com.example.bilabonnement.repositories;
 import java.sql.*;
 
 public class SQL_Manager {
+
+    private ResultSet rs;
+    private Statement stmt;
+
+
     public Statement establishConnection(){
-        String url = "";
-        String user = "";
-        String password = "";
+        String url = "jdbc:mysql://localhost:3306/Bilabonnement";
+        String user = "root";
+        String password = "testtest";
         try {
             Connection con = DriverManager.getConnection(url, user, password);
-            Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
             return stmt;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public boolean login(String username, String password) { //User can log in, and program registers credentials
+        try {
+            rs = stmt.executeQuery("SELECT * FROM users ORDER BY username");
+            while (rs.next()) {
+                if (rs.getString("username").equals(username)) {
+                    System.out.println("user found");
+                    if (rs.getString("password").equals(password)) {
+                        System.out.println("password matches");
+                        return true;
+                    } else {
+                        System.out.println("invalid password");
+                        return false;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("error?");
+        }
+        return false;
+    }
+
+    public void start() {
+        establishConnection();
     }
 }
