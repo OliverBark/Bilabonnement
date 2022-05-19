b10414d39464b37cf21846a0311b4956d6e041b8
Rasha <rash0081@kea.stud.dk>
2022-05-19 12:54:56 +0200

    commit

--- src/main/java/com/example/bilabonnement/services/damage/DamageService.java
+++ src/main/java/com/example/bilabonnement/services/damage/DamageService.java
@@ -11,6 +11,10 @@ import java.util.ArrayList;
 
 public class DamageService {
 
+
+    //Indsæt en damage liste med skaderne samt priserne aftalt i matrix
+    //Indsæt en damage liste med skaderne registreret efter aflevering i registered
+    //Tager kun stilling til priserne i matrix, registered kan være -1, 0 eller 1000, det er ligegyldigt
     public double calculateDamagePrice(ArrayList<Damage> matrix, ArrayList<Damage> registered){
         ArrayList<Damage> result = new ArrayList<>();
         for (Damage damage : matrix) {
