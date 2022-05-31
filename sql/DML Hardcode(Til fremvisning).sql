9996dcea752cf355a784e31f146ba06a8ce19511
Shaundator <rash0081@kea.stud.dk>
2022-05-31 13:28:37 +0200

    ændret model til carModel 2

--- sql/DML Hardcode(Til fremvisning).sql
+++ sql/DML Hardcode(Til fremvisning).sql
@@ -9,16 +9,16 @@ VALUES('James', 'James', 'test2', 'test2', 'test2', 5678, 56789123);
 INSERT INTO Customers (`first_name`,`last_name`, `address`, `mobile`, `cpr_nr`, `reg_nr`, `account_nr`)
 VALUES('Adam', 'Adam', 'test3', 'test3', 'test3', 9123, 45678912);
 
-INSERT INTO pending_rentals(`cpr_nr`,`model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
+INSERT INTO pending_rentals(`cpr_nr`,`car_model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
 VALUES('test1', 'test1', 'test1', 1, 1, 'test1', 1000);
 
-INSERT INTO pending_rentals(`cpr_nr`,`model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
+INSERT INTO pending_rentals(`cpr_nr`,`car_model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
 VALUES('test2', 'test2', 'test2', 0, 0, 'test2', 100);
 
-INSERT INTO pending_rentals(`cpr_nr`,`model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
+INSERT INTO pending_rentals(`cpr_nr`,`car_model`,`color`,`afleveringsforsikring`,`selvrisiko`,`location`,`monthly_fee`)
 VALUES('test3', 'test3', 'test3', 1, 0, 'test1', 500);
 
-INSERT INTO Rentals (cpr_nr, model, color, afleveringsforsikring, selvrisiko, location, price_pr_km, start_date, end_date, monthly_fee, active)
+INSERT INTO Rentals (cpr_nr, car_model, color, afleveringsforsikring, selvrisiko, location, price_pr_km, start_date, end_date, monthly_fee, active)
 VALUES('test3','test','test',1,1,'test',100,'2010-01-01','2015-01-01', 150, 1);
 
 
